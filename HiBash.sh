2696b553dfe86cf0011e515ba1987c0753ba0aa4
fbell884 <fbell884@gmail.com>
2020-09-21 17:16:39 -0400

    committing hi bash script

--- HiBash.sh
+++ HiBash.sh
@@ -4,16 +4,24 @@ echo "Here is 'Hi' in ASCII Stickmen"
 echo ""
 
 echo "  o         o      o     "
+sleep 1
 echo " <|>       <|>   _<|>_   "
+sleep 1
 echo " < >       < >           "
+sleep 1
 echo "  |         |      o     "
+sleep 1
 echo "  o__/_ _\__o     <|>    "
+sleep 1
 echo "  |         |     / \    "
+sleep 1
 echo " <o>       <o>    \o/    "
+sleep 1
 echo "  |         |      |     "
+sleep 1
 echo " / \       / \    / \    "
 
-sleep 4
+sleep 2
 clear
 echo "I hope you enjoyed!"
 sleep 2
