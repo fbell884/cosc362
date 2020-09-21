77a859073e8f6a9b7f535d752b0b602610307c44
fbell884 <fbell884@users.noreply.github.com>
2020-09-21 17:07:58 -0400

    committing ASCII bash

---
+++ HiBash.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Here is 'Hi' in ASCII Stickmen"
+echo ""
+
+echo "  o         o      o     "
+echo " <|>       <|>   _<|>_   "
+echo " < >       < >           "
+echo "  |         |      o     "
+echo "  o__/_ _\__o     <|>    "
+echo "  |         |     / \    "
+echo " <o>       <o>    \o/    "
+echo "  |         |      |     "
+echo " / \       / \    / \    "
+
+sleep 4
+clear
+echo "I hope you enjoyed!"
+sleep 2
+clear
+                      
+                      
+                      
